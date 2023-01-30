27115a87cb01e52e2e55c4285741299e945ada27
Yandex05 <voigva@yandex.ru>
2023-01-30 16:22:31 +0300

    добавились кнопки: Потерятся Карта Спутник Смешаные обработка не ведётся

--- main.py
+++ main.py
@@ -45,6 +45,18 @@ class Example(QWidget):
         self.image.setPixmap(self.pixmap)
         self.line1 = QLineEdit(self)
         self.line1.setGeometry(10, 10, 150, 30)
+        self.baton_search = QPushButton(self)
+        self.baton_search.setGeometry(170, 10, 80, 30)
+        self.baton_search.setText('Потерятся')
+        self.baton_map = QPushButton(self)
+        self.baton_map.setGeometry(250, 10, 40, 30)
+        self.baton_map.setText('Карта')
+        self.baton_sat = QPushButton(self)
+        self.baton_sat.setGeometry(290, 10, 60, 30)
+        self.baton_sat.setText('Спутник')
+        self.baton_mixed = QPushButton(self)
+        self.baton_mixed.setGeometry(350, 10, 70, 30)
+        self.baton_mixed.setText('Смешаные')
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
